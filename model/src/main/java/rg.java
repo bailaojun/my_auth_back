55f589b17a7a346caf361dec35bed3039e2611dd
baixiaojun <2447205703@qq.com>
2023-07-07 23:08:12 +0800

    Initial commit

---
+++ model/src/main/java/rg.java
@@ -0,0 +1,13 @@
+/**
+ * rg
+ *
+ * @author BaiXiaoJun
+ * @version 1.0
+ * @description
+ * @date 2023/7/7 22:26
+ */
+public class rg {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
